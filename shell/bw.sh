5f87c0637340df4b53283f05990908a556d66de7
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2010-09-27 13:39:05 +0000

    change bin/

--- shell/bw.sh
+++ shell/bw.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 自动换桌面背景
+
 bwname="1.jpg"
 swapname="swapname98924"
 photodir=""
@@ -8,36 +10,36 @@ photodir=""
 # do_inturn arg0
 do_inturn()
 {
-if [ -e "$1" ]; then
-    cat $1 |
-    while read line
-    do
-	if [ -z "$line"  ]; then  # 空行
-	    continue
-	fi
-        if [ "$line" == "$bwname" ]; then
-            continue
-        fi
+    if [ -e "$1" ]; then
+        cat $1 |
+        while read line
+        do
+            if [ -z "$line"  ]; then  # 空行
+                continue
+            fi
+            if [ "$line" == "$bwname" ]; then
+                continue
+            fi
 
-        echo $line
-        mv "$bwname" "$swapname"
-        mv "$line" "$bwname"
-        mv "$swapname" "$line"
+            echo $line
+            mv "$bwname" "$swapname"
+            mv "$line" "$bwname"
+            mv "$swapname" "$line"
 
-        sleep 600
-    done
-else
-    echo "找不到文件 $1"
-    exit 1
-fi
+            sleep 600
+        done
+    else
+        echo "找不到文件 $1"
+        exit 1
+    fi
 }
 
 # main {
-    #cd "$2"
-    while [ 0 -eq 0 ]
-    do
-        do_inturn "$1"
-    done
+#cd "$2"
+while [ 0 -eq 0 ]
+do
+    do_inturn "$1"
+done
 
-    exit 0
+exit 0
 # }
--- shell/rc.d/rc.local.sh
+++ shell/rc.d/rc.local.sh
@@ -4,7 +4,7 @@
 # run in /etc/rc.d/rc.local file.
 
 ## ruijie nat ...
-# ifconfig eth0 hw ether 00:1E:90:13:e0:25
+# ifconfig eth2 hw ether 00:1E:90:13:e0:25
     # ifconfig eth2 hw ether 00:1B:38:A1:8C:B9
     # service network restart
     # ifconfig virbr0 down
@@ -12,22 +12,31 @@
     # # /usr/local/nat/nat.sh
     # /usr/local/nat/killgxrj.sh&
 
-#ifconfig eth0 hw ether 00:1f:c6:11:ea:90 # ljx
+#ifconfig eth2 hw ether 00:1f:c6:11:ea:90 # ljx
 ifconfig eth2 hw ether 00:1b:38:a1:8c:b9 # gx
 service network restart
 ifconfig virbr0 down
 mentohust -b3
 iptables -F
 /usr/local/nat/nat.sh
-#/usr/local/nat/killgxrj.sh&
+# /usr/local/nat/killgxrj.sh&
 
 
 ## for VM's routeing
 route add -net 172.18.13.0/24 gw 10.3.1.29
 
 ## for IPv6
-ifconfig eth0 add 2001:250:1800:1::4/64   #ipv6 地址
-route -A inet6 add default gw 2001:250:1800:1::29 #ipv6 网关
+ifconfig eth2 add 2001:250:1800:1::4/64   # ipv6 地址
+route -A inet6 add default gw 2001:250:1800:1::29 # ipv6 网关
+
+## 重起锐锐
+rj=`ps aux | grep rj_restart.sh | grep -v grep`
+if [[ ! $rj ]]; then
+    eval /home/scr/bin/rj_restart.sh&
+fi
 
 ## for svnserve
-svnserve -d -r /var/svn
+svn=`ps aux | grep svnserve | grep -v grep`
+if [[ ! $svn ]]; then
+    svnserve -d -r /var/svn
+fi
