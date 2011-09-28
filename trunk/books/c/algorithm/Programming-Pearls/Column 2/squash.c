0a0ac83874d2714a68f1e0f4bb15567cc0fd7ffe
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-09-28 06:01:44 +0000

    高效查找同位词

--- trunk/books/c/algorithm/Programming-Pearls/Column 2/squash.c
+++ trunk/books/c/algorithm/Programming-Pearls/Column 2/squash.c
@@ -11,7 +11,8 @@
 #define WORDMAX 100
 
 int main()
-{   char word[WORDMAX], sig[WORDMAX], oldsig[WORDMAX];
+{
+    char word[WORDMAX], sig[WORDMAX], oldsig[WORDMAX];
     int linenum = 0;
     strcpy(oldsig, "");
     while (scanf("%s %s", sig, word) != EOF) {
