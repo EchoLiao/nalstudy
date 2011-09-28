e989d3151132cd57f31618f7a6ac158a9ed7ada5
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-09-28 01:59:09 +0000

    使用<bit>对特殊需求进行排序(高效)

--- books/c/algorithm/Programming-Pearls/Column 1/bitsort.c
+++ books/c/algorithm/Programming-Pearls/Column 1/bitsort.c
@@ -5,6 +5,8 @@
  *   Sort distinct integers in the range [0..N-1]
  */
 
+/* 所有的输入数据都是互不相同. */
+
 #include <stdio.h>
 
 #define BITSPERWORD 32
@@ -18,18 +20,19 @@ void clr(int i) {        a[i>>SHIFT] &= ~(1<<(i & MASK)); }
 int  test(int i){ return a[i>>SHIFT] &   (1<<(i & MASK)); }
 
 int main()
-{	int i;
-	for (i = 0; i < N; i++)
-		clr(i);
-/*	Replace above 2 lines with below 3 for word-parallel init
-	int top = 1 + N/BITSPERWORD;
-	for (i = 0; i < top; i++)
-		a[i] = 0;
- */
-	while (scanf("%d", &i) != EOF)
-		set(i);
-	for (i = 0; i < N; i++)
-		if (test(i))
-			printf("%d\n", i);
-	return 0;
+{	
+    int i;
+    for (i = 0; i < N; i++)
+        clr(i);
+    /*	Replace above 2 lines with below 3 for word-parallel init
+        int top = 1 + N/BITSPERWORD;
+        for (i = 0; i < top; i++)
+        a[i] = 0;
+        */
+    while (scanf("%d", &i) != EOF)
+        set(i);
+    for (i = 0; i < N; i++)
+        if (test(i))
+            printf("%d\n", i);
+    return 0;
 }
--- books/c/algorithm/Programming-Pearls/Column 1/sortints.cpp
+++ books/c/algorithm/Programming-Pearls/Column 1/sortints.cpp
@@ -8,9 +8,11 @@
 using namespace std;
 
 int main()
-{	set<int> S;
+{
+    set<int> S;
 	int i;
 	set<int>::iterator j;
+
 	while (cin >> i)
 		S.insert(i);
  	for (j = S.begin(); j != S.end(); ++j)
