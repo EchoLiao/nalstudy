610bda72e7c46a36518ff42d1d68a51e9c3f049c
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-09-28 06:01:44 +0000

    高效查找同位词

--- books/c/algorithm/Programming-Pearls/Column 2/sign.c
+++ books/c/algorithm/Programming-Pearls/Column 2/sign.c
@@ -11,14 +11,16 @@
 #define WORDMAX 100
 
 int charcomp(char *x, char *y)
-{   return *x - *y;
+{  
+    return *x - *y;
 }
 
 int main()
-{   char word[WORDMAX], sig[WORDMAX];
+{
+    char word[WORDMAX], sig[WORDMAX];
     while (scanf("%s", word) != EOF) {
         strcpy(sig, word);
-        qsort(sig, strlen(sig), sizeof(char), charcomp);
+        qsort(sig, strlen(sig), sizeof(char), (__compar_fn_t)charcomp);
         printf("%s %s\n", sig, word);
     }
     return 0;
