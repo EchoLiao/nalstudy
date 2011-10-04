0836d02a59da0620de9248994123efdee886c997
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-10-04 13:46:13 +0000

    some comments

--- trunk/books/c/cii/cii11/include/stack.h
+++ trunk/books/c/cii/cii11/include/stack.h
@@ -1,12 +1,24 @@
 /* $Id: H:/drh/idioms/book/RCS/inter.doc,v 1.11 1997/02/21 19:42:15 drh Exp $ */
 #ifndef STACK_INCLUDED
 #define STACK_INCLUDED
+
+/* 不提倡在接口文件中导入其它接口文件, 因为: [(P22)] */
+
+/* 1. 用T代替Stack_T使得接口更容易读懂;
+ * 2. #undef T 使得client只能使用Stack_T而T无效. */
 #define T Stack_T
+
+/* opaque pointer type: 在接口中只给出了结构体指针, 但不会揭示其内部结构, 实现
+ * 了内部细节的隐藏. */
 typedef struct T *T;
+
 extern T     Stack_new  (void);
+/* 若栈为空则返回1, 否则返回0. */
 extern int   Stack_empty(T stk);
 extern void  Stack_push (T stk, void *x);
 extern void *Stack_pop  (T stk);
 extern void  Stack_free (T *stk);
+
 #undef T
+
 #endif
--- trunk/books/c/cii/cii11/src/stack.c
+++ trunk/books/c/cii/cii11/src/stack.c
@@ -1,11 +1,14 @@
 static char rcsid[] =
-	"$Id: H:/drh/idioms/book/RCS/inter.doc,v 1.11 1997/02/21 19:42:15 drh Exp $";
+        "$Id: H:/drh/idioms/book/RCS/inter.doc,v 1.11 1997/02/21 19:42:15 drh Exp $";
 
 #include <stddef.h>
 #include "assert.h"
 #include "mem.h"
 #include "stack.h"
 
+/* 在头文件中有: typedef struct T *T; 
+ * 所以: T 即是 struct Stack_T* !
+ * */
 #define T Stack_T
 
 struct T
@@ -13,24 +16,27 @@ struct T
     int      count;
     struct elem
     {
-	void    *x;
-	struct elem *link;
-    }       *head;
+        void    *x;
+        struct elem *link;
+    }       *head;              // 永远指向栈顶
 };
 
 T Stack_new(void)
 {
     T        stk;
 
-    NEW(stk);
+    NEW(stk);                   // size is sizeof(*stk)
     stk->count = 0;
     stk->head = NULL;
     return stk;
 }
 
+/* 注意 const T stk 与 const struct T *stk 的区别!
+ * 在这不用 const 的原因: [(P29)] 
+ * */
 int Stack_empty(T stk)
 {
-    assert(stk);
+    assert(stk);                // checked runtime error
     return stk->count == 0;
 }
 
@@ -41,6 +47,7 @@ void Stack_push(T stk, void *x)
     assert(stk);
     NEW(t);
     t->x = x;
+    /* 在链表头插入 */
     t->link = stk->head;
     stk->head = t;
     stk->count++;
@@ -63,14 +70,13 @@ void    *Stack_pop(T stk)
 
 void Stack_free(T * stk)
 {
-    struct elem *t,
-            *u;
+    struct elem *t, *u;
 
     assert(stk && *stk);
     for (t = (*stk)->head; t; t = u)
     {
-	u = t->link;
-	FREE(t);
+        u = t->link;
+        FREE(t);                // 释放栈元素
     }
-    FREE(*stk);
+    FREE(*stk);                 // 释放栈自己
 }
