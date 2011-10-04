b5024627af8e79f3a77c5ee67bcdd0cd1acbbff4
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-10-04 13:45:46 +0000

    some comments

--- books/c/cii/cii11/src/arith.c
+++ books/c/cii/cii11/src/arith.c
@@ -13,14 +13,41 @@ int Arith_min(int x, int y)
     return x > y ? y : x;
 }
 
+/*==========================================================================*
+ * @Description: Arith_div() is the maximum integer that does not exceed the
+ *   real number z such that z*y=x.
+ *               it always truncate toward the left on the number line.
+ * 
+ * @Param x
+ * @Param y
+ * 
+ * @Returns:   
+ * 
+ *==========================================================================*/
 int Arith_div(int x, int y)
 {
+    /* 在编译期间, "-13 / 5 == -2" 已被求出真假, 若为真则说明编译器对"x / y"的
+     * 取舍所采用的原则是: 向0方向截断, 故返回值应该是"x / y - 1" */
     if (-13 / 5 == -2 && (x < 0) != (y < 0) && x % y != 0)
 	return x / y - 1;
     else
 	return x / y;
 }
 
+/*==========================================================================*
+ * @Description:    Arith_mod() is defined to be equal to x-y*Arith_div(x,y).
+ *      In if statement: 
+ *      Arith_mod(x, y) = x - y * Arith_div(x, y)
+ *                      = x - y * (x / y - 1)
+ *                      = x - y * (x / y) + y
+ *                      = x % y + y
+ *
+ * @Param x
+ * @Param y
+ *
+ * @Returns:    
+ *
+ *==========================================================================*/
 int Arith_mod(int x, int y)
 {
     if (-13 / 5 == -2 && (x < 0) != (y < 0) && x % y != 0)
@@ -29,11 +56,31 @@ int Arith_mod(int x, int y)
 	return x % y;
 }
 
+/*==========================================================================*
+ * @Description:    Arith_floor() returns the greatest integer not exceeding 
+ *      the real quotient of x/y.
+ *
+ * @Param x
+ * @Param y
+ *
+ * @Returns:    
+ *
+ *==========================================================================*/
 int Arith_floor(int x, int y)
 {
     return Arith_div(x, y);
 }
 
+/*==========================================================================*
+ * @Description:    Arith_ceiling() returns the least integer not less than 
+ *      the real quotient of x/y.
+ *
+ * @Param x
+ * @Param y
+ *
+ * @Returns:    
+ *
+ *==========================================================================*/
 int Arith_ceiling(int x, int y)
 {
     return Arith_div(x, y) + (x % y != 0);
