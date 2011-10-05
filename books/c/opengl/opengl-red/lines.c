21208fa824bd3117a27a0f5527ff59132b1d04fe
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-10-05 06:09:20 +0000

    chaper 3

--- books/c/opengl/opengl-red/lines.c
+++ books/c/opengl/opengl-red/lines.c
@@ -49,94 +49,107 @@
 #include <stdlib.h>
 
 #define drawOneLine(x1,y1,x2,y2)  glBegin(GL_LINES);  \
-   glVertex2f ((x1),(y1)); glVertex2f ((x2),(y2)); glEnd();
+    glVertex2f ((x1),(y1)); glVertex2f ((x2),(y2)); glEnd();
 
 void init(void)
 {
-   glClearColor (0.0, 0.0, 0.0, 0.0);
-   glShadeModel (GL_FLAT);
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glShadeModel (GL_FLAT);
 }
 
 void display(void)
 {
-   int i;
+    int i;
 
-   glClear (GL_COLOR_BUFFER_BIT);
+    glClear (GL_COLOR_BUFFER_BIT);
 
-/* select white for all lines  */
-   glColor3f (1.0, 1.0, 1.0);
+    /* select white for all lines  */
+    glColor3f (1.0, 1.0, 1.0);
 
-/* in 1st row, 3 lines, each with a different stipple  */
-   glEnable (GL_LINE_STIPPLE);
+    /* in 1st row, 3 lines, each with a different stipple  */
+    /* =============================================================== */
+    glEnable (GL_LINE_STIPPLE); /* 开启直线的点画模式 */
 
-   glLineStipple (1, 0x0101);  /*  dotted  */
-   drawOneLine (50.0, 125.0, 150.0, 125.0);
-   glLineStipple (1, 0x00FF);  /*  dashed  */
-   drawOneLine (150.0, 125.0, 250.0, 125.0);
-   glLineStipple (1, 0x1C47);  /*  dash/dot/dash  */
-   drawOneLine (250.0, 125.0, 350.0, 125.0);
+    /* glLineStipple (1, 0x0101);  [> dotted <] */
+    glLineStipple (1, 0xAAAA);  /*  dotted  */
+    /* drawOneLine (50.0, 125.0, 150.0, 125.0); */
+    glLineStipple (1, 0x00FF);  /*  dashed  */
+    /* drawOneLine (150.0, 125.0, 250.0, 125.0); */
+    glLineStipple (1, 0x1C47);  /*  dash/dot/dash  */
+    /* 0001 1100 0100 0111     1110 0010 0011 1000
+     * 1110001000111000111000100011100011100010001110001110001000111000
+     * ---   -   ---   ---   -   ---   ---   -   ---   ---   -   ---
+     * ^               ^               ^               ^
+     */
+    drawOneLine (250.0, 125.0, 350.0, 125.0);
 
-/* in 2nd row, 3 wide lines, each with different stipple */
-   glLineWidth (5.0);
-   glLineStipple (1, 0x0101);  /*  dotted  */
-   drawOneLine (50.0, 100.0, 150.0, 100.0);
-   glLineStipple (1, 0x00FF);  /*  dashed  */
-   drawOneLine (150.0, 100.0, 250.0, 100.0);
-   glLineStipple (1, 0x1C47);  /*  dash/dot/dash  */
-   drawOneLine (250.0, 100.0, 350.0, 100.0);
-   glLineWidth (1.0);
+    /* in 2nd row, 3 wide lines, each with different stipple */
+    /* =============================================================== */
+    glLineWidth (5.0);
+    glLineStipple (1, 0x0101);  /*  dotted  */
+    drawOneLine (50.0, 100.0, 150.0, 100.0);
+    glLineStipple (1, 0x00FF);  /*  dashed  */
+    drawOneLine (150.0, 100.0, 250.0, 100.0);
+    glLineStipple (1, 0x1C47);  /*  dash/dot/dash  */
+    drawOneLine (250.0, 100.0, 350.0, 100.0);
+    glLineWidth (1.0);
 
-/* in 3rd row, 6 lines, with dash/dot/dash stipple  */
-/* as part of a single connected line strip         */
-   glLineStipple (1, 0x1C47);  /*  dash/dot/dash  */
-   glBegin (GL_LINE_STRIP);
-   for (i = 0; i < 7; i++)
-      glVertex2f (50.0 + ((GLfloat) i * 50.0), 75.0);
-   glEnd ();
+    /* in 3rd row, 6 lines, with dash/dot/dash stipple  */
+    /* =============================================================== */
+    /* as part of a single connected line strip         */
+    glLineStipple (1, 0x1C47);  /*  dash/dot/dash  */
+    glBegin (GL_LINE_STRIP);
+    /* glBegin (GL_LINES); [>各个点不相邻画出的线是断续的<] */
+    for (i = 0; i < 7; i++)
+        glVertex2f (50.0 + ((GLfloat) i * 50.0), 75.0);
+    glEnd ();
 
-/* in 4th row, 6 independent lines with same stipple  */
-   for (i = 0; i < 6; i++) {
-      drawOneLine (50.0 + ((GLfloat) i * 50.0), 50.0,
-         50.0 + ((GLfloat)(i+1) * 50.0), 50.0);
-   }
+    /* in 4th row, 6 independent lines with same stipple  */
+    /* =============================================================== */
+    for (i = 0; i < 6; i++) { /* NOT: i < 7 */
+        drawOneLine (50.0 + ((GLfloat) i * 50.0), 50.0,
+                50.0 + ((GLfloat)(i+1) * 50.0), 50.0);
+    }
 
-/* in 5th row, 1 line, with dash/dot/dash stipple    */
-/* and a stipple repeat factor of 5                  */
-   glLineStipple (5, 0x1C47);  /*  dash/dot/dash  */
-   drawOneLine (50.0, 25.0, 350.0, 25.0);
+    /* in 5th row, 1 line, with dash/dot/dash stipple    */
+    /* =============================================================== */
+    /* and a stipple repeat factor of 5                  */
+    /* glDisable (GL_LINE_STIPPLE); */
+    glLineStipple (5, 0x1C47);  /*  dash/dot/dash  */
+    drawOneLine (50.0, 25.0, 350.0, 25.0);
 
-   glDisable (GL_LINE_STIPPLE);
-   glFlush ();
+    glDisable (GL_LINE_STIPPLE);
+    glFlush ();
 }
 
 void reshape (int w, int h)
 {
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ();
-   gluOrtho2D (0.0, (GLdouble) w, 0.0, (GLdouble) h);
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    gluOrtho2D (0.0, (GLdouble) w, 0.0, (GLdouble) h);
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-   switch (key) {
-      case 27:
-         exit(0);
-         break;
-   }
+    switch (key) {
+        case 27:
+            exit(0);
+            break;
+    }
 }
 
 int main(int argc, char** argv)
 {
-   glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-   glutInitWindowSize (400, 150);
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow (argv[0]);
-   init ();
-   glutDisplayFunc(display);
-   glutReshapeFunc(reshape);
-   glutKeyboardFunc(keyboard);
-   glutMainLoop();
-   return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (400, 150);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    init ();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
 }
