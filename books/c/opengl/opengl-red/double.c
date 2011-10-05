68ecde7e95e76824d9efcc0462d6697dbd5cc108
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-10-05 06:09:04 +0000

    charper 2

--- books/c/opengl/opengl-red/double.c
+++ books/c/opengl/opengl-red/double.c
@@ -53,55 +53,58 @@ static GLfloat spin = 0.0;
 
 void display(void)
 {
-   glClear(GL_COLOR_BUFFER_BIT);
-   glPushMatrix();
-   glRotatef(spin, 0.0, 0.0, 1.0);
-   glColor3f(1.0, 1.0, 1.0);
-   glRectf(-25.0, -25.0, 25.0, 25.0);
-   glPopMatrix();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+    {
+        glRotatef(spin, 0.0, 0.0, 1.0);
+        glColor3f(1.0, 1.0, 1.0);
+        glRectf(-25.0, -25.0, 25.0, 25.0);
+    }
+    glPopMatrix();
 
-   glutSwapBuffers();
+    glutSwapBuffers(); /* 交换缓冲区 */
 }
 
 void spinDisplay(void)
 {
-   spin = spin + 2.0;
-   if (spin > 360.0)
-      spin = spin - 360.0;
-   glutPostRedisplay();
+    spin = spin + 2.0;
+    if (spin > 360.0)
+        spin = spin - 360.0;
+    glutPostRedisplay(); /* 触发display()被调用 */
 }
 
 void init(void)
 {
-   glClearColor (0.0, 0.0, 0.0, 0.0);
-   glShadeModel (GL_FLAT);
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glShadeModel (GL_FLAT);
 }
 
 void reshape(int w, int h)
 {
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+    /* QQQQQ */
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 void mouse(int button, int state, int x, int y)
 {
-   switch (button) {
-      case GLUT_LEFT_BUTTON:
-         if (state == GLUT_DOWN)
-            glutIdleFunc(spinDisplay);
-         break;
-      case GLUT_MIDDLE_BUTTON:
-      case GLUT_RIGHT_BUTTON:
-         if (state == GLUT_DOWN)
-            glutIdleFunc(NULL);
-         break;
-      default:
-         break;
-   }
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if (state == GLUT_DOWN) /* 注册空闲时被调用的函数 */
+                glutIdleFunc(spinDisplay);
+            break;
+        case GLUT_MIDDLE_BUTTON:
+        case GLUT_RIGHT_BUTTON:
+            if (state == GLUT_DOWN)
+                glutIdleFunc(NULL);
+            break;
+        default:
+            break;
+    }
 }
 
 /*
@@ -110,15 +113,15 @@ void mouse(int button, int state, int x, int y)
  */
 int main(int argc, char** argv)
 {
-   glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (250, 250);
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow (argv[0]);
-   init ();
-   glutDisplayFunc(display);
-   glutReshapeFunc(reshape);
-   glutMouseFunc(mouse);
-   glutMainLoop();
-   return 0;   /* ANSI C requires main to return int. */
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize (250, 250);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    init ();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(mouse);
+    glutMainLoop();
+    return 0;   /* ANSI C requires main to return int. */
 }
