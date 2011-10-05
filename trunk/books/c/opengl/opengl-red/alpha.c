10d2bd1918b020bff24de2ccfa0209b39e5f7a7a
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-10-05 06:12:29 +0000

    验证 glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA) 所产生的混合效果(三张图混合). [(P157 E)]

--- trunk/books/c/opengl/opengl-red/alpha.c
+++ trunk/books/c/opengl/opengl-red/alpha.c
@@ -50,6 +50,9 @@
 #include <stdlib.h>
 
 static int leftFirst = GL_TRUE;
+static GLfloat clR = 1.0, clG = 0.2, clB = 0.3, clA = 0.0;
+static GLfloat lfR = 1.0, lfG = 1.0, lfB = 0.0, lfA = 0.35;
+static GLfloat rtR = 0.0, rtG = 1.0, rtB = 1.0, rtA = 0.45;
 
 /*  Initialize alpha blending function.
  */
@@ -58,7 +61,7 @@ static void init(void)
    glEnable (GL_BLEND);
    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
    glShadeModel (GL_FLAT);
-   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glClearColor (clR, clG, clB, clA);
 }
 
 static void drawLeftTriangle(void)
@@ -66,11 +69,79 @@ static void drawLeftTriangle(void)
    /* draw yellow triangle on LHS of screen */
 
    glBegin (GL_TRIANGLES);
-      glColor4f(1.0, 1.0, 0.0, 0.75);
+      glColor4f(lfR, lfG, lfB, lfA);
       glVertex3f(0.1, 0.9, 0.0);
       glVertex3f(0.1, 0.1, 0.0);
       glVertex3f(0.7, 0.5, 0.0);
    glEnd();
+}
+
+
+/* 
+ * Des:
+ *     验证 glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA) 在该场景所产生
+ *     的混合效果. 
+ *
+ *     本程序的场景等价于三张图(background, leftTriangle, rightTriangle)的混合
+ *     [(P157 E)].
+ *
+ *     绘图顺序: background -> leftTriangle -> rightTriangle
+ */
+static void drawCheckTriangle_BLR(void)
+{
+   glDisable(GL_BLEND);
+   glPushMatrix ();
+
+   glScalef(0.5, 0.5, 0.5);
+   glTranslated(0.4, 0.2, 0.0);
+   /* leftTriangle 和 background 混合等到 left-back */
+   GLfloat lR = lfR * lfA + clR * (1.0 - lfA);
+   GLfloat lG = lfG * lfA + clG * (1.0 - lfA);
+   GLfloat lB = lfB * lfA + clB * (1.0 - lfA);
+   GLfloat lA = lfA * lfA + clA * (1.0 - lfA);
+   /* 然后: rightTriangle 和 left-back 混合 */
+   GLfloat tR = rtR * rtA + lR * (1.0 - rtA);
+   GLfloat tG = rtG * rtA + lG * (1.0 - rtA);
+   GLfloat tB = rtB * rtA + lB * (1.0 - rtA);
+   GLfloat tA = rtA * rtA + lA * (1.0 - rtA);
+   glBegin (GL_TRIANGLES);
+      glColor4f(tR, tG, tB, tA);
+      glVertex3f(0.45, 0.9, 0.0);
+      glVertex3f(-0.15, 0.5, 0.0);
+      glVertex3f(0.45, 0.1, 0.0);
+   glEnd();
+
+   glPopMatrix ();
+   glEnable(GL_BLEND);
+}
+
+/* 绘图顺序: background -> rightTriangle -> leftTriangle
+ */
+static void drawCheckTriangle_BRL(void)
+{
+   glDisable(GL_BLEND);
+   glPushMatrix ();
+
+   glScalef(0.5, 0.5, 0.5);
+   glTranslated(0.4, 0.2, 0.0);
+   glRotatef(-90.0, 0.0, 0.0, 1.0);
+   GLfloat rR = rtR * rtA + clR * (1.0 - rtA);
+   GLfloat rG = rtG * rtA + clG * (1.0 - rtA);
+   GLfloat rB = rtB * rtA + clB * (1.0 - rtA);
+   GLfloat rA = rtA * rtA + clA * (1.0 - rtA);
+   GLfloat tR = lfR * lfA + rR * (1.0 - lfA);
+   GLfloat tG = lfG * lfA + rG * (1.0 - lfA);
+   GLfloat tB = lfB * lfA + rB * (1.0 - lfA);
+   GLfloat tA = lfA * lfA + rA * (1.0 - lfA);
+   glBegin (GL_TRIANGLES);
+      glColor4f(tR, tG, tB, tA);
+      glVertex3f(0.3, 0.5, 0.0);
+      glVertex3f(-0.3, 0.1, 0.0);
+      glVertex3f(0.3, -0.3, 0.0);
+   glEnd();
+
+   glPopMatrix ();
+   glEnable(GL_BLEND);
 }
 
 static void drawRightTriangle(void)
@@ -78,7 +149,7 @@ static void drawRightTriangle(void)
    /* draw cyan triangle on RHS of screen */
 
    glBegin (GL_TRIANGLES);
-      glColor4f(0.0, 1.0, 1.0, 0.75);
+      glColor4f(rtR, rtG, rtB, rtA);
       glVertex3f(0.9, 0.9, 0.0);
       glVertex3f(0.3, 0.5, 0.0);
       glVertex3f(0.9, 0.1, 0.0);
@@ -89,6 +160,8 @@ void display(void)
 {
    glClear(GL_COLOR_BUFFER_BIT);
 
+   glPushMatrix ();
+   glScalef(0.5, 0.5, 0.5);
    if (leftFirst) {
       drawLeftTriangle();
       drawRightTriangle();
@@ -97,6 +170,10 @@ void display(void)
       drawRightTriangle();
       drawLeftTriangle();
    }
+   glPopMatrix ();
+
+   drawCheckTriangle_BLR();
+   drawCheckTriangle_BRL();
 
    glFlush();
 }
@@ -110,6 +187,10 @@ void reshape(int w, int h)
       gluOrtho2D (0.0, 1.0, 0.0, 1.0*(GLfloat)h/(GLfloat)w);
    else
       gluOrtho2D (0.0, 1.0*(GLfloat)w/(GLfloat)h, 0.0, 1.0);
+
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glTranslatef(0.0, 0.3, 0.0);
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -136,7 +217,8 @@ int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-   glutInitWindowSize (200, 200);
+   glutInitWindowSize (600, 600);
+   glutInitWindowPosition (500, 10);
    glutCreateWindow (argv[0]);
    init();
    glutReshapeFunc (reshape);
