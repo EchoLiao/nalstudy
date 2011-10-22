9167df47050392a2e2ea661120b7cd1f49d24ae4
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-10-22 03:59:36 +0000

    增加三个用于同步".rt"和".rt-notes"目录的脚本

---
+++ shell/rt2rtnote.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+#===============================
+# Modify follow line to fit you!
+rt=/home/scr/.rt
+rtnt=/home/scr/.rt-notes
+fdir="address-book gs life linux system-install-notes win"
+
+tf=/tmp/find.fileex.tmp.132422
+
+
+[[ $1 == "-h" ]] && {
+    echo "Usage:"
+    echo "  `basename $0`"
+    echo
+    echo "Description:"
+    echo "  把 $rt/{$fdir}"
+    echo "  目录及子目录下的所有 *.txt, README, INSTALL 文件移动到 $rtnt 目录下,"
+    echo "  相应的目录结构保持不变, 并在 $rt 中作出相应的软链接."
+    echo
+    exit 69
+}
+
+cd "$rt"
+if [[ $? == 0 ]]; then
+    #===============================
+    # Modify follow line to fit you!
+    echo "THE BASE DIRECTORY IS: $rt/{$fdir}"
+    find $fdir -type f \
+            -iname "*.txt" -o -iname "README" -o -iname "INSTALL" \
+            -iname "README.*" -o -iname "INSTALL.*" \
+            | grep -v '.git' | grep -v '.svn' \
+            | grep -v '/rfc/' | grep -v 'softEngineering' > $tf
+else
+    echo "Can't cd to $rt"
+    exit 1
+fi
+
+cat $tf |
+while read line
+do
+    if [ -z "$line"  ]; then continue; fi
+    if [[ "$line" == "." ]]; then continue; fi
+    bname=${line##*/}   # bname=`basename "$line"`
+    bpath=${line%/*}    # bpath=${line/%\/$bname/}
+    #echo $bpath KKKKKKKKKKKKKKKKKK $bname
+
+    if [[ -L "$rt/$line" ]]; then
+        if [[ -f "$rtnt/$line" ]]; then continue; else
+            echo "THEY ARE NOT EQUAL 1: $rt/$line AND $rtnt/$line" 1>&2; continue;
+        fi
+    else
+        if [[ ! -e "$rtnt/$line" ]]; then :; else # Move it late
+            echo "THEY ARE NOT EQUAL 2: $rt/$line AND $rtnt/$line" 1>&2; continue;
+        fi
+    fi
+
+    # Move the file ...
+    [[ ! -e "$rtnt/$bpath" ]] && mkdir -p "$rtnt/$bpath"
+    echo "mv $rt/$bpath/$bname $rtnt/$bpath/$bname"
+    mv "$rt/$bpath/$bname" "$rtnt/$bpath/"
+    [[ $? == 0 ]] && {
+        ln -s "$rtnt/$bpath/$bname" "$rt/$bpath/$bname"
+    }
+
+    #sleep 1
+done
+
+exit 0
---
+++ shell/rtlinkisvalid.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+rt=/home/scr/.rt
+rtnt=/home/scr/.rt-notes
+fdir="address-book gs life linux system-install-notes win"
+fd=
+isfindall=0
+tf=/tmp/find.fileex.tmp.132422
+
+function Usage()
+{
+    echo "Usage:"
+    echo "  `basename $0` [-a] [-h] [dir]"
+    echo "      -a      Find all files"
+    echo "      -h      Disply this message"
+    echo "      dir     The directory want to look up"
+    echo
+    echo "Description:"
+    echo "  查找某一目录下是否存在坏的软链接."
+    echo "  (若不带任何参数, 则查找 $rt/{$fdir}"
+    echo "   目录及子目录下的所有 *.txt, README, INSTALL 文件)"
+    exit 69
+}
+
+parm=`getopt "ha" "$@"`
+[[ $? != 0 ]] &&  Usage
+
+# "set --" : 重新设置命令行参数
+set -- $parm
+
+while :
+do
+    case "$1" in
+        -a) isfindall=1;;
+        -h) Usage;;
+        --) break ;;
+    esac
+    shift
+done
+shift    # Remove the trailing --
+
+if [[ "$#" == 0 || "$#" == 1 ]]; then fd="$*"; else Usage; fi
+
+# 若没有提供要查找的目录, 则默认使用 /home/scr/.rt 作为查找的基础目录.
+fd=${fd:-$rt}
+
+
+cd "$fd"
+if [[ $? == 0 ]]; then
+    if [[ "$fd" == "$rt" || "$fd" == "$rtnt" ]]; then
+        fdir="address-book gs life linux system-install-notes win"
+    else
+        fdir="."
+    fi
+    echo "THE BASE DIRECTORY IS: $fd/{$fdir}"
+    if [[ $isfindall == 1 ]]; then
+        find $fdir -type l > $tf
+    else
+        find $fdir -type l \
+                -iname "*.txt" -o -iname "README" -o -iname "INSTALL" \
+                -iname "README.*" -o -iname "INSTALL.*" \
+                | grep -v '.git' | grep -v '.svn' \
+                | grep -v '/rfc/' | grep -v 'softEngineering' > $tf
+    fi
+else
+    echo "Can't cd to $fd"
+    exit 1
+fi
+
+cat $tf |
+while read line
+do
+    if [ -z "$line"  ]; then continue; fi
+    if [[ "$line" == "." ]]; then continue; fi
+    bname=${line##*/}   # bname=`basename "$line"`
+    bpath=${line%/*}    # bpath=${line/%\/$bname/}
+    #echo $bpath KKKKKKKKKKKKKKKKKK $bname
+
+    # 测试该软链接所指向的文件是否存在
+    if [ ! -e "$fd/$bpath/$bname" ]; then
+        echo "CAN NOT FIND THE LINKPOINT OF: $fd/$bpath/$bname" 1>&2
+    fi
+    #### 下面的代码也能实现这一功能.
+    #xxd -l 1 "$fd/$bpath/$bname" > /dev/null 2>&1   # 测试该软链接所指向的文件是否存在
+    #[[ $? != 0 ]] && {
+    #    echo "CAN NOT FIND THE LINKPOINT OF: $fd/$bpath/$bname" 1>&2
+    #}
+done
+
+exit 0
