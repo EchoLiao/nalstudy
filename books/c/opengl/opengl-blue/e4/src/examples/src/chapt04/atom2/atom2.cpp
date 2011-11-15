8d57601e0a5c8609daa13d5730bb948d1f451900
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-11-15 10:36:04 +0000

    indent and makefile

--- books/c/opengl/opengl-blue/e4/src/examples/src/chapt04/atom2/atom2.cpp
+++ books/c/opengl/opengl-blue/e4/src/examples/src/chapt04/atom2/atom2.cpp
@@ -16,114 +16,114 @@ static GLfloat yRot = 0.0f;
 
 // Called to draw scene
 void RenderScene(void)
-	{
-	// Angle of revolution around the nucleus
-	static float fElect1 = 0.0f;
+{
+    // Angle of revolution around the nucleus
+    static float fElect1 = 0.0f;
 
-	// Clear the window with current clearing color
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Red Nucleus
-	glColor3ub(255, 0, 0);
-	glutSolidSphere(10.0f, 15, 15);
+    // Red Nucleus
+    glColor3ub(255, 0, 0);
+    glutSolidSphere(10.0f, 15, 15);
 
-	// Yellow Electrons
-	glColor3ub(255,255,0);
+    // Yellow Electrons
+    glColor3ub(255,255,0);
 
-	// First Electron Orbit
-	// Save viewing transformation
-	glPushMatrix();
+    // First Electron Orbit
+    // Save viewing transformation
+    glPushMatrix(); {
 
-	// Rotate by angle of revolution
-	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+        // Rotate by angle of revolution
+        glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
 
-	// Translate out from origin to orbit distance
-	glTranslatef(90.0f, 0.0f, 0.0f);
+        // Translate out from origin to orbit distance
+        glTranslatef(90.0f, 0.0f, 0.0f);
 
-	// Draw the electron
-	glutSolidSphere(6.0f, 15, 15);
+        // Draw the electron
+        glutSolidSphere(6.0f, 15, 15);
 
-	// Restore the viewing transformation
-	glPopMatrix();
+        // Restore the viewing transformation
+    } glPopMatrix();
 
-	// Second Electron Orbit
-	glPushMatrix();
-	glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-	glTranslatef(-70.0f, 0.0f, 0.0f);
-	glutSolidSphere(6.0f, 15, 15);
-	glPopMatrix();
-
-
-	// Third Electron Orbit
-	glPushMatrix();
-	glRotatef(360.0f-45.0f,0.0f, 0.0f, 1.0f);
-	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-	glTranslatef(0.0f, 0.0f, 60.0f);
-	glutSolidSphere(6.0f, 15, 15);
-	glPopMatrix();
+    // Second Electron Orbit
+    glPushMatrix(); {
+        glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
+        glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+        glTranslatef(-70.0f, 0.0f, 0.0f);
+        glutSolidSphere(6.0f, 15, 15);
+    } glPopMatrix();
 
 
-	// Increment the angle of revolution
-	fElect1 += 10.0f;
-	if(fElect1 > 360.0f)
-		fElect1 = 0.0f;
+    // Third Electron Orbit
+    glPushMatrix(); {
+        glRotatef(360.0f-45.0f,0.0f, 0.0f, 1.0f);
+        glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+        glTranslatef(0.0f, 0.0f, 60.0f);
+        glutSolidSphere(6.0f, 15, 15);
+    } glPopMatrix();
 
-	// Show the image
-	glutSwapBuffers();
-	}
+
+    // Increment the angle of revolution
+    fElect1 += 10.0f;
+    if(fElect1 > 360.0f)
+        fElect1 = 0.0f;
+
+    // Show the image
+    glutSwapBuffers();
+}
 
 
 // This function does any needed initialization on the rendering
-// context. 
+// context.
 void SetupRC()
-	{
-	glEnable(GL_DEPTH_TEST);	// Hidden surface removal
-	glFrontFace(GL_CCW);		// Counter clock-wise polygons face out
-	glEnable(GL_CULL_FACE);		// Do not calculate inside of jet
+{
+    glEnable(GL_DEPTH_TEST);	// Hidden surface removal
+    glFrontFace(GL_CCW);		// Counter clock-wise polygons face out
+    glEnable(GL_CULL_FACE);		// Do not calculate inside of jet
 
-	// Black background
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f );	
-    }
+    // Black background
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
+}
 
 void SpecialKeys(int key, int x, int y)
-	{
-	if(key == GLUT_KEY_UP)
-		xRot-= 5.0f;
+{
+    if(key == GLUT_KEY_UP)
+        xRot-= 5.0f;
 
-	if(key == GLUT_KEY_DOWN)
-		xRot += 5.0f;
+    if(key == GLUT_KEY_DOWN)
+        xRot += 5.0f;
 
-	if(key == GLUT_KEY_LEFT)
-		yRot -= 5.0f;
+    if(key == GLUT_KEY_LEFT)
+        yRot -= 5.0f;
 
-	if(key == GLUT_KEY_RIGHT)
-		yRot += 5.0f;
+    if(key == GLUT_KEY_RIGHT)
+        yRot += 5.0f;
 
-	if(key > 356.0f)
-		xRot = 0.0f;
+    if(key > 356.0f)
+        xRot = 0.0f;
 
-	if(key < -1.0f)
-		xRot = 355.0f;
+    if(key < -1.0f)
+        xRot = 355.0f;
 
-	if(key > 356.0f)
-		yRot = 0.0f;
+    if(key > 356.0f)
+        yRot = 0.0f;
 
-	if(key < -1.0f)
-		yRot = 355.0f;
+    if(key < -1.0f)
+        yRot = 355.0f;
 
-	// Refresh the Window
-	glutPostRedisplay();
-	}
+    // Refresh the Window
+    glutPostRedisplay();
+}
 
 void TimerFunc(int value)
-    {
+{
     glutPostRedisplay();
     glutTimerFunc(100, TimerFunc, 1);
-    }
+}
 
 void ChangeSize(int w, int h)
-    {
+{
     GLfloat fAspect;
 
     // Prevent a divide by zero
@@ -143,20 +143,20 @@ void ChangeSize(int w, int h)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glTranslatef(0.0f, 0.0f, -250.0f);
-    }
+}
 
 int main(int argc, char* argv[])
-	{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-        glutInitWindowSize(800, 600);
-	glutCreateWindow("OpenGL Atom - Part Duex");
-	glutReshapeFunc(ChangeSize);
-	glutSpecialFunc(SpecialKeys);
-	glutDisplayFunc(RenderScene);
-        glutTimerFunc(500, TimerFunc, 1);
-	SetupRC();
-	glutMainLoop();
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("OpenGL Atom - Part Duex");
+    glutReshapeFunc(ChangeSize);
+    glutSpecialFunc(SpecialKeys);
+    glutDisplayFunc(RenderScene);
+    glutTimerFunc(500, TimerFunc, 1);
+    SetupRC();
+    glutMainLoop();
 
-	return 0;
-	}
+    return 0;
+}
