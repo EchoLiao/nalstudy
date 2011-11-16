9ab79862ec95bfbbb3417b5aa4690553b6540eaf
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-11-16 02:33:07 +0000

    indent and makefile

--- trunk/books/c/opengl/opengl-blue/e4/src/examples/src/chapt04/transform/transform.cpp
+++ trunk/books/c/opengl/opengl-blue/e4/src/examples/src/chapt04/transform/transform.cpp
@@ -12,19 +12,19 @@
 
 // Draw a torus (doughnut), using the current 1D texture for light shading
 void DrawTorus(M3DMatrix44f mTransform)
-    {
+{
     GLfloat majorRadius = 0.35f;
     GLfloat minorRadius = 0.15f;
     GLint   numMajor = 40;
     GLint   numMinor = 20;
     M3DVector3f objectVertex;         // Vertex in object/eye space
-    M3DVector3f transformedVertex;    // New Transformed vertex   
+    M3DVector3f transformedVertex;    // New Transformed vertex
     double majorStep = 2.0f*M3D_PI / numMajor;
     double minorStep = 2.0f*M3D_PI / numMinor;
     int i, j;
-    
-    for (i=0; i<numMajor; ++i) 
-        {
+
+    for (i=0; i<numMajor; ++i)
+    {
         double a0 = i * majorStep;
         double a1 = a0 + majorStep;
         GLfloat x0 = (GLfloat) cos(a0);
@@ -33,8 +33,8 @@ void DrawTorus(M3DMatrix44f mTransform)
         GLfloat y1 = (GLfloat) sin(a1);
 
         glBegin(GL_TRIANGLE_STRIP);
-        for (j=0; j<=numMinor; ++j) 
-            {
+        for (j=0; j<=numMinor; ++j)
+        {
             double b = j * minorStep;
             GLfloat c = (GLfloat) cos(b);
             GLfloat r = minorRadius * c + majorRadius;
@@ -53,59 +53,57 @@ void DrawTorus(M3DMatrix44f mTransform)
             objectVertex[2] = z;
             m3dTransformVector3(transformedVertex, objectVertex, mTransform);
             glVertex3fv(transformedVertex);
-            }
-        glEnd();
         }
+        glEnd();
     }
-        
-        
+}
+
+
 // Called to draw scene
 void RenderScene(void)
-    {
+{
     M3DMatrix44f   transformationMatrix;   // Storeage for rotation matrix
     static GLfloat yRot = 0.0f;         // Rotation angle for animation
     yRot += 0.5f;
-        
+
     // Clear the window with current clearing color
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
     // Build a rotation matrix
     m3dRotationMatrix44(transformationMatrix, m3dDegToRad(yRot), 0.0f, 1.0f, 0.0f);
     transformationMatrix[12] = 0.0f;
     transformationMatrix[13] = 0.0f;
     transformationMatrix[14] = -2.5f;
-        
+
     DrawTorus(transformationMatrix);
 
     // Do the buffer Swap
     glutSwapBuffers();
-    }
+}
 
 // This function does any needed initialization on the rendering
-// context. 
+// context.
 void SetupRC()
-    {
+{
     // Bluish background
     glClearColor(0.0f, 0.0f, .50f, 1.0f );
-         
+
     // Draw everything as wire frame
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    }
+}
 
 ///////////////////////////////////////////////////////////
 // Called by GLUT library when idle (window not being
 // resized or moved)
 void TimerFunction(int value)
-    {
+{
     // Redraw the scene with new coordinates
     glutPostRedisplay();
     glutTimerFunc(33,TimerFunction, 1);
-    }
-
-
+}
 
 void ChangeSize(int w, int h)
-    {
+{
     GLfloat fAspect;
 
     // Prevent a divide by zero, when window is too short
@@ -114,22 +112,22 @@ void ChangeSize(int w, int h)
         h = 1;
 
     glViewport(0, 0, w, h);
-        
+
     fAspect = (GLfloat)w / (GLfloat)h;
 
     // Reset the coordinate system before modifying
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-	
+
     // Set the clipping volume
     gluPerspective(35.0f, fAspect, 1.0f, 50.0f);
-        
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    }
+}
 
 int main(int argc, char* argv[])
-    {
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800,600);
@@ -143,4 +141,4 @@ int main(int argc, char* argv[])
     glutMainLoop();
 
     return 0;
-    }
+}
