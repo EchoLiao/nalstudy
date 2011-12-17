35205dbfd6dba60cdcc6809613e045d41371b63a
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-12-17 17:59:43 +0000

    实现ftp批量上传, 会自动创建目录(包括多层)

--- shell/ftp-up-down.sh
+++ shell/ftp-up-down.sh
@@ -2,21 +2,87 @@
 
 # 批量自动上传文件脚本
 
-for filename in *
+slist=/tmp/tmp_updown_ftp_23433435
+stest=tmp/jjj/hhh/kkkk/aaa/bbb/tmp_updown_ftp_23433435
+ftpsh1=~/.ftp_updown_tmp_3232222_tmp_1.sh
+ftpsh2=~/.ftp_updown_tmp_3232222_tmp_2.sh
+npwd=`pwd -P`
+
+do_create_mklist()
+{
+    tpathres=$1
+    tlastname=${1##*/}
+    prvpath=.
+
+    cat /dev/null > $2
+    for (( ; 0 == 0; ))
+    do
+        if [[ $tpathres == $tlastname ]]; then break; fi
+        st=${tpathres%%/*}
+        tpathres=${tpathres#*/}
+        prvpath=$prvpath/$st
+        #echo $st
+        #echo $tpathres
+        echo "mkdir $prvpath" >> $2
+        #echo 
+    done
+}
+
+
+find . -type f | grep -v '.swp' | grep -v '.swo' > $slist
+
+echo "Start ... ..."
+
+cat $slist |
+while read line
 do
 
-ftp -n -i -v 221.1.217.92 <<END
-user nuoerlz xxxxxx
-binary
-hash
-cd /
-lcd /home/scr/.rt-notes/linux/computor/grath/opengl/note
-prompt
-mput $filename
-bye
-END
+if [ -z "$line"  ]; then continue; fi
+sdir=${line%/*}
+sfile=${line##*/}
+sdir=`echo $sdir | sed 's:^./::'`
+line=`echo $line | sed 's:^./::'`
+#echo $line
+#echo $sdir KKKKKKKKKK
+#echo $sfile LLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+#echo
+
+echo ========================================================
+echo "ftp -n -i -v 221.1.217.92 << END"    >  $ftpsh1
+echo "user nuoerlz xxxxxx"                 >> $ftpsh1
+echo "binary"                              >> $ftpsh1
+echo "hash"                                >> $ftpsh1
+echo "cd /"                                >> $ftpsh1
+do_create_mklist $line $ftpsh2               
+cat $ftpsh2                                >> $ftpsh1
+echo "cd /$sdir"                           >> $ftpsh1
+echo "lcd $npwd/$sdir"                     >> $ftpsh1
+echo "prompt"                              >> $ftpsh1
+echo "mput $sfile"                         >> $ftpsh1
+echo "bye"                                 >> $ftpsh1
+echo "END"                                 >> $ftpsh1
+
+/bin/sh $ftpsh1
+
+rm -f $ftpsh1
+
+
+#ftp -n -i -v 221.1.217.92 <<END
+#user nuoerlz google
+#binary
+#hash
+#cd /
+#mkdir -p /$sdir
+#cd /$sdir
+#lcd $npwd/$sdir
+#prompt
+#mput $sfile
+#bye
+#END
 
 done
+
+echo "Done !!"
 
 exit 0
 
