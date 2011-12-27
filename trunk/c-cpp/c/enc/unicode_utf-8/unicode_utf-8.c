92cb025091dc6df670542e88d9f82fa884720ba1
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2011-12-27 10:53:01 +0000

    几个 Unicode 与 UTF-8 相互转换的接口

--- trunk/c-cpp/c/enc/unicode_utf-8/unicode_utf-8.c
+++ trunk/c-cpp/c/enc/unicode_utf-8/unicode_utf-8.c
@@ -33,20 +33,16 @@
                                    ^^   ++++++   ++====   ====--   ------
  * U-04000000 - U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
                                     ^   ^^^^^^   ++++++   ++====   ====--   ------
- * 
+ *
  *  (前面几个1就代表后面几个字节是属于一起的)
- *  
+ *
  */
 
-
-/*
- * Unicode有二字节编码(UCS-2)和四字节编码(UCS-4)两种, 目前常用的二字节编码. 下
- * 面程序给出的是UTF-8转成Unicode(UCS-2)的函数.
- */
 
 
 #include <string.h>
 #include <assert.h>
+
 
 
 /*****************************************************************************
@@ -54,160 +50,88 @@
  * 字节空间. 特殊地, 对于只占一个字节的字符(ASCII), 返回值为 0 .
  *
  * 参数:
- *    char c    字符的UTF-8编码的第一个字节的值
+ *    c    字符的UTF-8编码的第一个字节的值
  *
  * 返回值:
  *   该字符用UTF-8编码存储时所需要多少个字节空间.
  *   特殊地, 对于只占一个字节的字符(ASCII), 返回值为 0 .
  ****************************************************************************/
-int enc_get_utf8_size(char c)
+int enc_get_utf8_size(unsigned char c)
 {
-    char t = 1 << 7;
-    char r = c;
+    unsigned char t = 0x80;
+    unsigned char r = c;
     int count = 0;
-    while (r & t)
+
+    while ( r & t )
     {
         r = r << 1;
         count++;
     }
+
     return count;
 }
 
-
 /*****************************************************************************
- * 将UTF8编码转换成Unicode(UCS-2)编码.
+ * 将一个字符的UTF8编码转换成Unicode(UCS-2和UCS-4)编码.
  *
  * 参数:
- *    char* pInput   指向输入字符串(以'\0'结尾)的指针
- *    char* pOutput  指向输出字符串指针的指针
- *    int   outsize  pOutput 缓冲的大小
+ *    pInput      指向输入缓冲区, 以UTF-8编码
+ *    Unic        指向输出缓冲区, 其保存的数据即是Unicode编码值,
+ *                类型为unsigned long .
  *
  * 返回值:
- *    返回转换后的Unicode字符串的字节数, 如果出错则返回 -1
+ *    成功则返回该字符的UTF8编码所占用的字节数; 失败则返回0.
  *
  * 注意:
- *     1. UTF8没有字节序问题, 但是Unicode字符有字节序,
- *        字节序分为大端(Big Endian)和小端(Little Endian)两种,
- *        在Intel处理器中采用小端法表示, 本例中采用小端法表示. (低地址存低位)
- *     2. 请保证 pOutput 缓冲区有足够的空间, 若空间不足, 则只进行部分转换.
- ****************************************************************************/
-int enc_utf8_to_unicode(const char* pInput, char* pOutput, int outsize)
-{
-    assert(pInput != NULL && pOutput != NULL);
-    assert(outsize >= 3);
-
-    int  outputSize = 0;  //记录转换后的Unicode字符串的字节数
-    char *pout = pOutput;
-
-    while ( *pInput && outputSize + 2 + 1 <= outsize )
-    {
-        if (*pInput > 0x00 && *pInput <= 0x7F) // 处理单字节UTF8字符
-        {
-            *pout     = *pInput;
-            *(pout+1) = 0;       // 小端法表示, 在高地址填补0
-            pInput += 1;
-        }
-        else if (((*pInput) & 0xE0) == 0xC0) // 处理双字节UTF8字符
-        {
-            char high = *pInput;
-            char low  = *(pInput + 1);
-
-            if ((low & 0xC0) != 0x80) // 检查是否为合法的UTF8字符表示
-                return -1;
-
-            *pout     = (high << 6) + (low & 0x3F);
-            *(pout+1) = (high >> 2) & 0x07;
-            pInput += 2;
-        }
-        else if (((*pInput) & 0xF0) == 0xE0) // 处理三字节UTF8字符
-        {
-            char high   = *pInput;
-            char middle = *(pInput + 1);
-            char low    = *(pInput + 2);
-
-            if (((middle & 0xC0) != 0x80) || ((low & 0xC0) != 0x80))
-                return -1;
-
-            *pout     = (middle << 6) + (low & 0x3F);
-            *(pout+1) = (high << 4) + ((middle >> 2) & 0x0F);
-            pInput += 3;
-        }
-        else //对于其他字节数的UTF8字符不进行处理
-        {
-            return -1;
-        }
-
-        pout += 2;
-        outputSize += 2;
-    }
-
-    *pout = 0;
-
-    return outputSize;
-}
-
-#if 1
-/*****************************************************************************
- * 将UTF8编码转换成Unicode(UCS-2和UCS-4)编码.
- *
- * 参数:
- *    char* pInput   指向输入字符串, 以UTF-8编码
- *    char* pOutput  指向输出字符串, 以Unicode编码
- *
- * 返回值:
- *    返回转换后的字符的Unicode编码所占的字节数, 如果出错则返回 -1 .
- *
- * 注意:
- *     1. UTF8没有字节序问题, 但是Unicode字符有字节序,
- *        字节序分为大端(Big Endian)和小端(Little Endian)两种,
+ *     1. UTF8没有字节序问题, 但是Unicode有字节序要求;
+ *        字节序分为大端(Big Endian)和小端(Little Endian)两种;
  *        在Intel处理器中采用小端法表示, 在此采用小端法表示. (低地址存低位)
- *     2. 请保证 pOutput 缓冲区有最少有 4 字节的空间大小!
  ****************************************************************************/
-int enc_utf8_to_unicode_one(const char* pInput, char* pOutput)
+int enc_utf8_to_unicode_one(const unsigned char* pInput, unsigned long *Unic)
 {
-    assert(pInput != NULL && pOutput != NULL);
+    assert(pInput != NULL && Unic != NULL);
 
     // b1 表示UTF-8编码的pInput中的高字节, b2 表示次高字节, ...
     char b1, b2, b3, b4, b5, b6;
+
+    *Unic = 0x0; // 把 *Unic 初始化为全零
     int utfbytes = enc_get_utf8_size(*pInput);
+    unsigned char *pOutput = (unsigned char *) Unic;
 
     switch ( utfbytes )
     {
         case 0:
             *pOutput     = *pInput;
-            return 1;
+            utfbytes    += 1;
             break;
         case 2:
             b1 = *pInput;
             b2 = *(pInput + 1);
             if ( (b2 & 0xE0) != 0x80 )
-                return -1;
+                return 0;
             *pOutput     = (b1 << 6) + (b2 & 0x3F);
             *(pOutput+1) = (b1 >> 2) & 0x07;
-            return 2;
             break;
         case 3:
             b1 = *pInput;
             b2 = *(pInput + 1);
             b3 = *(pInput + 2);
             if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80) )
-                return -1;
+                return 0;
             *pOutput     = (b2 << 6) + (b3 & 0x3F);
             *(pOutput+1) = (b1 << 4) + ((b2 >> 2) & 0x0F);
-            return 2;
             break;
         case 4:
             b1 = *pInput;
             b2 = *(pInput + 1);
             b3 = *(pInput + 2);
             b4 = *(pInput + 3);
-            if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80) 
+            if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80)
                     || ((b4 & 0xC0) != 0x80) )
-                return -1;
+                return 0;
             *pOutput     = (b3 << 6) + (b4 & 0x3F);
             *(pOutput+1) = (b2 << 4) + ((b3 >> 2) & 0x0F);
             *(pOutput+2) = ((b1 << 2) & 0x1C)  + ((b2 >> 4) & 0x03);
-            return 3;
             break;
         case 5:
             b1 = *pInput;
@@ -215,14 +139,13 @@ int enc_utf8_to_unicode_one(const char* pInput, char* pOutput)
             b3 = *(pInput + 2);
             b4 = *(pInput + 3);
             b5 = *(pInput + 4);
-            if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80) 
+            if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80)
                     || ((b4 & 0xC0) != 0x80) || ((b5 & 0xC0) != 0x80) )
-                return -1;
+                return 0;
             *pOutput     = (b4 << 6) + (b5 & 0x3F);
             *(pOutput+1) = (b3 << 4) + ((b4 >> 2) & 0x0F);
             *(pOutput+2) = (b2 << 2) + ((b3 >> 4) & 0x03);
             *(pOutput+3) = (b1 << 6);
-            return 4;
             break;
         case 6:
             b1 = *pInput;
@@ -231,34 +154,212 @@ int enc_utf8_to_unicode_one(const char* pInput, char* pOutput)
             b4 = *(pInput + 3);
             b5 = *(pInput + 4);
             b6 = *(pInput + 5);
-            if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80) 
-                    || ((b4 & 0xC0) != 0x80) || ((b5 & 0xC0) != 0x80) 
+            if ( ((b2 & 0xC0) != 0x80) || ((b3 & 0xC0) != 0x80)
+                    || ((b4 & 0xC0) != 0x80) || ((b5 & 0xC0) != 0x80)
                     || ((b6 & 0xC0) != 0x80) )
-                return -1;
+                return 0;
             *pOutput     = (b5 << 6) + (b6 & 0x3F);
             *(pOutput+1) = (b5 << 4) + ((b6 >> 2) & 0x0F);
             *(pOutput+2) = (b3 << 2) + ((b4 >> 4) & 0x03);
             *(pOutput+3) = ((b1 << 6) & 0x40) + (b2 & 0x3F);
-            return 4;
             break;
         default:
-            assert(0 && "pInput is NOT a UTF-8 encoding!");
+            return 0;
             break;
     }
 
-    return -1;
+    return utfbytes;
 }
 
-int enc_utf8_to_unicode_one_uint(const char* pInput, unsigned int *Output)
+/*****************************************************************************
+ * 将字符串的UTF8编码转换成Unicode(UCS-2和UCS-4)编码.
+ *
+ * 参数:
+ *    pInput      指向输入缓冲区, 以UTF-8编码
+ *    nMembIn     pInput大小(即, 字符串的长度)(单位: 1字节)
+ *    pOutput     指向输出缓冲区, 用于保存Unicode编码值
+ *    nMembOut    输入: pOutput可以存储的Unicode编码(字符)的个数(单位: 4字节);
+ *                输出: 成功转换的字符的个数.
+ *
+ * 返回值:
+ *    若有错误发生, 则返回 0 ;
+ *    若所有的字符都转换成功, 则返回 1 ;
+ *    若输出缓冲区空间不足, 导致只有部分字符转换成功, 则返回 2 ;
+ *    另: *nMembOut 返回成功转换的字符的个数.
+ *
+ * 注意:
+ *     1. UTF8没有字节序问题, 但是Unicode有字节序要求;
+ *        字节序分为大端(Big Endian)和小端(Little Endian)两种;
+ *        在Intel处理器中采用小端法表示, 在此采用小端法表示. (低地址存低位)
+ ****************************************************************************/
+int enc_utf8_to_unicode(const unsigned char* pInput, int nMembIn,
+        unsigned long* pOutput, int* nMembOut)
 {
-    int          ret;
+    assert(pInput != NULL && pOutput != NULL);
+    assert(*nMembOut >= 1);
 
-    assert(sizeof(*Output) >= 4);
+    int i, ret, outSize;
+    const unsigned char *pIn     = pInput;
+    unsigned long       *pOutCur = pOutput;
 
-    *Output = 0x0; // 把 *Output 初始化为全零
-    ret = enc_utf8_to_unicode_one(pInput, (char *)Output);
+    outSize = *nMembOut;
+    for ( i = 0; i < nMembIn; )
+    {
+        if ( pOutCur - pOutput >= outSize )
+        {
+            *nMembOut = pOutCur - pOutput;
+            return 2; // 输出空间不足
+        }
 
-    return ret;
+        ret = enc_utf8_to_unicode_one(pIn, pOutCur);
+        if ( ret == 0 )
+        {
+            *nMembOut = pOutCur - pOutput;
+            return 0; // 输入的字符编码不是UTF8
+        }
+
+        i    += ret;
+        pIn  += ret;
+        pOutCur += 1;
+    }
+
+    *nMembOut = pOutCur - pOutput;
+    return 1;         // 所有字符转换成功
 }
 
-#endif
+
+/*****************************************************************************
+ * 将一个字符的Unicode(UCS-2和UCS-4)编码转换成UTF-8编码.
+ *
+ * 参数:
+ *    unic     字符的Unicode编码值
+ *    pOutput  指向输出的用于存储UTF8编码值的缓冲区的指针
+ *    outsize  pOutput缓冲的大小
+ *
+ * 返回值:
+ *    返回转换后的字符的UTF8编码所占的字节数, 如果出错则返回 0 .
+ *
+ * 注意:
+ *     1. UTF8没有字节序问题, 但是Unicode有字节序要求;
+ *        字节序分为大端(Big Endian)和小端(Little Endian)两种;
+ *        在Intel处理器中采用小端法表示, 在此采用小端法表示. (低地址存低位)
+ *     2. 请保证 pOutput 缓冲区有最少有 6 字节的空间大小!
+ ****************************************************************************/
+int enc_unicode_to_utf8_one(unsigned long unic, unsigned char *pOutput,
+        int outSize)
+{
+    assert(pOutput != NULL);
+    assert(outSize >= 6);
+
+    if ( unic <= 0x0000007F )
+    {
+        // * U-00000000 - U-0000007F:  0xxxxxxx
+        *pOutput     = (unic & 0x7F);
+        return 1;
+    }
+    else if ( unic >= 0x00000080 && unic <= 0x000007FF )
+    {
+        // * U-00000080 - U-000007FF:  110xxxxx 10xxxxxx
+        *(pOutput+1) = (unic & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 6) & 0x1F) | 0xC0;
+        return 2;
+    }
+    else if ( unic >= 0x00000800 && unic <= 0x0000FFFF )
+    {
+        // * U-00000800 - U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
+        *(pOutput+2) = (unic & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >>  6) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 12) & 0x0F) | 0xE0;
+        return 3;
+    }
+    else if ( unic >= 0x00010000 && unic <= 0x001FFFFF )
+    {
+        // * U-00010000 - U-001FFFFF:  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+        *(pOutput+3) = (unic & 0x3F) | 0x80;
+        *(pOutput+2) = ((unic >>  6) & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >> 12) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 18) & 0x07) | 0xF0;
+        return 4;
+    }
+    else if ( unic >= 0x00200000 && unic <= 0x03FFFFFF )
+    {
+        // * U-00200000 - U-03FFFFFF:  111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+        *(pOutput+4) = (unic & 0x3F) | 0x80;
+        *(pOutput+3) = ((unic >>  6) & 0x3F) | 0x80;
+        *(pOutput+2) = ((unic >> 12) & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >> 18) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 24) & 0x03) | 0xF8;
+        return 5;
+    }
+    else if ( unic >= 0x04000000 && unic <= 0x7FFFFFFF )
+    {
+        // * U-04000000 - U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+        *(pOutput+5) = (unic & 0x3F) | 0x80;
+        *(pOutput+4) = ((unic >>  6) & 0x3F) | 0x80;
+        *(pOutput+3) = ((unic >> 12) & 0x3F) | 0x80;
+        *(pOutput+2) = ((unic >> 18) & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >> 24) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 30) & 0x01) | 0xFC;
+        return 6;
+    }
+
+    return 0;
+}
+
+/*****************************************************************************
+ * 将字符串的Unicode(UCS-2和UCS-4)编码转换成UTF8编码.
+ *
+ * 参数:
+ *    pInput      指向输入缓冲区, 以Unicode编码
+ *    nMembIn     pInput大小(即, 字符串的长度)(单位: 4字节)
+ *    pOutput     指向输出缓冲区, 用于保存UTF8编码值
+ *    nMembOut    输入: pOutput缓冲区的大小(单位: 1字节);
+ *                输出: 成功转换后, UTF8编码所占空间大小(单位: 1字节).
+ *
+ * 返回值:
+ *    若有错误发生, 则返回 0 ;
+ *    若所有的字符都转换成功, 则返回 1 ;
+ *    若输出缓冲区空间不足, 导致只有部分字符转换成功, 则返回 2 ;
+ *    另: *nMembOut返回UTF8编码所占空间大小(单位: 1字节).
+ *
+ * 注意:
+ *     1. UTF8没有字节序问题, 但是Unicode有字节序要求;
+ *        字节序分为大端(Big Endian)和小端(Little Endian)两种;
+ *        在Intel处理器中采用小端法表示, 在此采用小端法表示. (低地址存低位)
+ ****************************************************************************/
+int enc_unicode_to_utf8(const unsigned long *pInput, int nMembIn,
+        unsigned char *pOutput, int *nMembOut)
+{
+    assert(pInput != NULL && pOutput != NULL );
+    assert(*nMembOut >= 6);
+
+    int i, ret, outSize, resOutSize;
+    const unsigned long *pIn     = pInput;
+    unsigned char       *pOutCur = pOutput;
+
+    outSize = *nMembOut;
+    for ( i = 0; i < nMembIn; )
+    {
+        resOutSize = outSize - (pOutCur - pOutput);
+        if ( resOutSize < 6 )
+        {
+            *nMembOut = pOutCur - pOutput;
+            return 2;
+        }
+
+        ret = enc_unicode_to_utf8_one(*pIn, pOutCur, resOutSize);
+        if ( ret == 0 )
+        {
+            *nMembOut = pOutCur - pOutput;
+            return 0;
+        }
+
+        i   += 1;
+        pIn += 1;
+        pOutCur += ret;
+    }
+
+    *nMembOut = pOutCur - pOutput;
+    return 1;
+}
+
--- trunk/c-cpp/c/enc/unicode_utf-8/unicode_utf-8.h
+++ trunk/c-cpp/c/enc/unicode_utf-8/unicode_utf-8.h
@@ -23,6 +23,35 @@
 #ifndef UNICODE_UTF_8_H_
 #define UNICODE_UTF_8_H_
 
-int enc_utf8_to_unicode(const char* pInput, char* pOutput, int outsize);
+/*
+ * 根据字符的UTF-8编码的第一个字节求出该字符用UTF-8编码存储时所需要多少个
+ * 字节空间. 特殊地, 对于只占一个字节的字符(ASCII), 返回值为 0 .
+ * */
+int enc_get_utf8_size(unsigned char c);
+
+
+/*
+ * 将一个字符的UTF8编码转换成Unicode(UCS-2和UCS-4)编码.
+ * */
+int enc_utf8_to_unicode_one(const unsigned char *pInput, unsigned long *Unic);
+
+/*
+ * 将一个字符的Unicode(UCS-2和UCS-4)编码转换成UTF-8编码.
+ * */
+int enc_unicode_to_utf8_one(unsigned long unic, unsigned char *pOutput, int outSize);
+
+
+/*
+ * 将字符串的UTF8编码转换成Unicode(UCS-2和UCS-4)编码.
+ * */
+int enc_utf8_to_unicode(const unsigned char *pInput, int nMembIn,
+        unsigned long *pOutput, int *nMembOut);
+
+/*
+ * 将字符串的Unicode(UCS-2和UCS-4)编码转换成UTF8编码.
+ * */
+int enc_unicode_to_utf8(const unsigned long *pInput, int nMembIn,
+        unsigned char *pOutput, int *nMembOut);
+
 
 #endif // UNICODE_UTF_8_H_
