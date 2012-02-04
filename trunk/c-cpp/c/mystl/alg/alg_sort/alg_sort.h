50efabc4926c8c2f48daa8e2d9f289170fd6d990
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2012-02-04 10:26:45 +0000

    内部排序接口与实现:
        1. 插入排序;

---
+++ trunk/c-cpp/c/mystl/alg/alg_sort/alg_sort.h
@@ -0,0 +1,32 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  alg_sort.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012年02月04日 15时21分16秒
+ *        Created:  
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  nuoerlz (nuoliu), nuoerlz@gmail.com
+ *        Company:  mhtt
+ *
+ *      CopyRight:  Reserve
+ *
+ * ===========================================================================
+ */
+
+#ifndef ALG_SORT_H_
+#define ALG_SORT_H_
+
+
+/* 
+* 插入排序
+ */
+void Sort_insertSort(void *base, size_t nmemb, size_t size,
+        int(*compar)(const void *, const void *));
+
+#endif
---
+++ trunk/c-cpp/c/mystl/alg/alg_sort/alg_sort_test.h
@@ -0,0 +1,22 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  alg_sort_test.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012年02月04日 17时23分47秒
+ *        Created:  
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  nuoerlz (nuoliu), nuoerlz@gmail.com
+ *        Company:  mhtt
+ *
+ *      CopyRight:  Reserve
+ *
+ * ===========================================================================
+ */
+
+
