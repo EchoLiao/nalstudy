646385e7d384a4e3050be3f2cb4846198087dcdb
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2012-02-04 09:36:47 +0000

    语法错误

--- trunk/c-cpp/c/mystl/alg/alg_rand/alg_rand.c
+++ trunk/c-cpp/c/mystl/alg/alg_rand/alg_rand.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include "alg_rand.h"
 
 
 
--- trunk/c-cpp/c/mystl/alg/alg_rand/alg_rand.h
+++ trunk/c-cpp/c/mystl/alg/alg_rand/alg_rand.h
@@ -27,13 +27,13 @@
 /*
  * return a random number, it's value is in [a, b].
  */
-unsigned int Rand_int(unsigned int a, unsigned int b)
+unsigned int Rand_int(unsigned int a, unsigned int b);
 
 
 /*
  * 返回一个整形随机数组, 大小为n, 元素是唯一的, 且其元素值介于0和n-1之间.
  */
-int* Rand_randN(int n)
+int* Rand_randN(int n);
 
 
 #endif
