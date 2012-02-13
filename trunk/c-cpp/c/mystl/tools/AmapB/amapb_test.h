d86ccf057e5186854873680a25d3a04f4ac3834f
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2012-02-13 08:06:28 +0000

    通用映射工具

---
+++ trunk/c-cpp/c/mystl/tools/AmapB/amapb_test.h
@@ -0,0 +1,22 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  amapb_test.h
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2012年02月13日 13时59分26秒
+ *        Created:
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  nuoerlz (nuoliu), nuoerlz@gmail.com
+ *        Company:  mhtt
+ *
+ *      CopyRight:  Reserve
+ *
+ * ===========================================================================
+ */
+
+
