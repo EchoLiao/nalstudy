d0523ca730c292ba5985d2666d34408b92cd35e7
nuoerlz <nuoerlz@da13e6bb-9c2c-a04c-5f4c-2d7d41371f5f>
2012-04-06 04:50:34 +0000

    未初始化的全局变量可以声明多次, 其是弱符号; 声明并初始化的只能有一次, 其是强符号

---
+++ c-cpp/c/test/global_val/e.h
@@ -0,0 +1,22 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  e.h
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2012年03月20日 17时49分19秒
+ *        Created:
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  nuoerlz (nuoliu), nuoerlz@gmail.com
+ *        Company:  mhtt
+ *
+ *      CopyRight:  Reserve
+ *
+ * ===========================================================================
+ */
+
+
